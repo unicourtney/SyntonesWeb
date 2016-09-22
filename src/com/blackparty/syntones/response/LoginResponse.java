74ff8c9d388b2426b53ce7ba8783feaeb3f7cea7
unicourtney <clpenaflorida@gmail.com>
2016-09-23 00:31:23 +0800

    Working basket recommendation

---
+++ src/com/blackparty/syntones/response/LoginResponse.java
@@ -0,0 +1,44 @@
+package com.blackparty.syntones.response;
+
+import java.util.List;
+
+import com.blackparty.syntones.model.Message;
+import com.blackparty.syntones.model.Playlist;
+import com.blackparty.syntones.model.User;
+
+
+public class LoginResponse {
+	
+	
+	
+	private Message message;
+	private List<Playlist> recentlyPlayedPlaylists;
+	
+	public LoginResponse(){}
+
+	
+	public LoginResponse(User user, Message message, List<Playlist> recentlyPlayedPlaylists){
+		this.message = message;
+		this.recentlyPlayedPlaylists = recentlyPlayedPlaylists;
+	}
+	
+	
+	public Message getMessage() {
+		return message;
+	}
+
+	public void setMessage(Message message) {
+		this.message = message;
+	}
+
+	public List<Playlist> getRecentlyPlayedPlaylists() {
+		return recentlyPlayedPlaylists;
+	}
+
+	public void setRecentlyPlayedPlaylists(List<Playlist> recentPlaylistsPlayed) {
+		this.recentlyPlayedPlaylists = recentPlaylistsPlayed;
+	}
+
+
+		
+}
